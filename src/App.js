f3351bdcfb0c4444d3ae5ab54a91ba5d9ba055d3
Timur Kunayev <tk@connectedleasing.com>
2017-10-01 21:51:12 -0600

    extract render into a separate function/class

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import './App.css';
 import { DataLoader, Submitter } from './DataLoader.js'
 
 
+const render = ( loading, result ) => (
+  <div>
+    <h1>{ loading ? "Loading..." : result }&nbsp;</h1>
+    <Submitter>{(onSubmit) => ( <a className={loading ? 'disabled' : '' } onClick={() => loading ? null :onSubmit("Deal 1")}>Load Deal 1</a>)}</Submitter>
+    <Submitter>{(onSubmit) => ( <a className={loading ? 'disabled' : ''} onClick={() => loading ? null : onSubmit("Deal 2")}>Load Deal 2</a>)}</Submitter>
+  </div>
+)
+
 class App extends Component {
   render() {
     return (
@@ -12,15 +20,8 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
           </header>
-          <DataLoader render={ (loading, result) =>
-            (
-              <div>
-                <h1>{ loading ? "Loading..." : result }&nbsp;</h1>
-                <Submitter>{(onSubmit) => ( <a className={loading ? 'disabled' : '' } onClick={() => loading ? null :onSubmit("Deal 1")}>Load Deal 1</a>)}</Submitter>
-                <Submitter>{(onSubmit) => ( <a className={loading ? 'disabled' : ''} onClick={() => loading ? null : onSubmit("Deal 2")}>Load Deal 2</a>)}</Submitter>
-              </div>
-            )
-          }/><br />
+          <DataLoader render={ (loading, result) => render(loading, result) } />
+          <br />
       </div>
     );
   }
